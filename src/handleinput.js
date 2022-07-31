0ba6674c2e8e57503f857888f3e6cd4d94a607cc
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-07-31 15:10:55 -0400

    F to C toggle working

--- src/handleinput.js
+++ src/handleinput.js
@@ -1,22 +1,28 @@
 import { updateApp } from "./index.js";
+
+const toggleSwitch = document.querySelector("[data-unit-toggle]");
+let unitsHandler = "imperial";
+
+// gets location input
 const locationHandler = () => {
   return "q=orlando";
 };
 
 // gets the input from the units toggle switch and returns imperial or metric
-const unitsHandler = () => {
-  const toggleSwitch = document.querySelector("[data-unit-toggle]");
-  let unit = "imperial";
-  toggleSwitch.addEventListener("change", (e) => {
-    if (e.target.checked) {
-      unit = "metric";
-    }
-    if (!e.target.checked) {
-      unit = "imperial";
-    }
-    console.log("After toggle switch:", unit);
-    updateApp();
-  });
-  return unit;
-};
-export { unitsHandler, locationHandler };
+// const unitsHandler = () => {
+toggleSwitch.addEventListener("change", (e) => {
+  e.stopImmediatePropagation();
+  if (e.target.checked) {
+    unitsHandler = "metric";
+  }
+  if (!e.target.checked) {
+    unitsHandler = "imperial";
+  }
+  // return unit;
+  updateApp();
+});
+
+// };
+
+// export { unitsHandler, locationHandler };
+export { locationHandler, unitsHandler };
--- src/index.js
+++ src/index.js
@@ -3,20 +3,16 @@ import { apiAddress } from "./api.js";
 import { locationHandler, unitsHandler } from "./handleinput.js";
 
 const apiRequest = async (inputLocation, units, appid) => {
-  console.log(unitsHandler());
-  console.log("Before API call:", units);
+  //   console.log(unitsHandler());
+  //   console.log("Before API call:", units);
   const sentRequest = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?${inputLocation}&appid=${appid}&units=${units}`
   );
   const gotRequest = await sentRequest.json();
   console.log(gotRequest);
 };
-
-const updateApp = () => {
-  apiRequest(locationHandler(), unitsHandler(), apiAddress);
-};
-
-// window.onload = () => {
+function updateApp() {
+  apiRequest(locationHandler(), unitsHandler, apiAddress);
+}
 updateApp();
-// };
 export { updateApp };
