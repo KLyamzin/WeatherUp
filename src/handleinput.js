a9d25d7f65550a8ab54871c889de9246412d6816
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-08-01 21:32:11 -0400

    location input and units are working together

--- src/handleinput.js
+++ src/handleinput.js
@@ -1,40 +1,58 @@
-import { updateApp } from "./index.js";
+// import { updateApp } from "./index.js";
+import { apiAddress } from "./api.js";
 
-const locationInputForm = document.querySelector("[data-location-form]");
-const locationInput = document.querySelector("[data-location-input]");
-// let locationInputText = document.querySelector("[data-location-input]").value;
-let locationHandler = "q=Orlando";
-const regexZip = new RegExp(
-  /^((\d{5}-\d{4})|(\d{5})|([A-Z]\d[A-Z]\s\d[A-Z]\d))$/
-);
-const regexCity = new RegExp(
-  /^([a-zA-Z\u0080-\u024F]+(?:(. )|-| |'))*[a-zA-Z\u0080-\u024F]*$/
-);
-// gets location input
-locationInputForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let locationInputText = document.querySelector("[data-location-input]").value;
-  if (regexZip.test(locationInputText)) {
-    locationHandler = `zip=${locationInputText}`;
-  } else if (regexCity.test(locationInputText)) {
-    locationHandler = `q=${locationInputText}`;
-  } else {
-    window.alert("Please enter a valid zip code or city");
-  }
-  updateApp();
-});
-// gets the input from the units toggle switch and returns imperial or metric
-const toggleSwitch = document.querySelector("[data-unit-toggle]");
-let unitsHandler = "imperial";
-toggleSwitch.addEventListener("change", (e) => {
-  e.stopImmediatePropagation();
-  if (e.target.checked) {
-    unitsHandler = "metric";
-  }
-  if (!e.target.checked) {
-    unitsHandler = "imperial";
-  }
-  updateApp();
-});
+const updateApp = (e) => {
+  // The API Call
+  const apiRequest = async (location, units, appid) => {
+    const sentRequest = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?${location}&appid=${appid}&units=${units}`
+    );
+    const gotRequest = await sentRequest.json();
+    console.log(gotRequest);
+    if (sentRequest.status === 404) {
+      window.alert(`Error: ${gotRequest.message}`);
+    }
+  };
 
-export { locationHandler, unitsHandler };
+  // gets location input
+  const locationHandler = () => {
+    let location = `q=Washington DC`; //default location
+    let locationInputText = document.querySelector(
+      "[data-location-input]"
+    ).value;
+    const regexZip = new RegExp(
+      /^((\d{5}-\d{4})|(\d{5})|([A-Z]\d[A-Z]\s\d[A-Z]\d))$/
+    );
+    const regexCity = new RegExp(
+      /^([a-zA-Z\u0080-\u024F]+(?:(. )|-| |'))*[a-zA-Z\u0080-\u024F]*$/
+    );
+    e.preventDefault();
+    if (locationInputText !== "") {
+      if (regexZip.test(locationInputText)) {
+        location = `zip=${locationInputText}`;
+      } else if (regexCity.test(locationInputText)) {
+        location = `q=${locationInputText}`;
+      } else {
+        window.alert("Please enter a valid zip code or city");
+      }
+      return location;
+    } else {
+      window.alert("Please enter a valid zip code or city");
+    }
+  };
+
+  // gets the input from the units toggle switch and returns imperial or metric
+  const unitsHandler = () => {
+    e.stopImmediatePropagation();
+    let units = "imperial";
+    if (e.target.checked) {
+      units = "metric";
+    }
+    if (!e.target.checked) {
+      units = "imperial";
+    }
+    return units;
+  };
+  apiRequest(locationHandler(), unitsHandler(), apiAddress);
+};
+export { /* locationHandler, unitsHandler, */ updateApp };
--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 import "./unitslider.css";
 import "./main.css";
-import { apiAddress } from "./api.js";
-import { locationHandler, unitsHandler } from "./handleinput.js";
+import { updateApp } from "./handleinput.js";
 
-const apiRequest = async (inputLocation, units, appid) => {
-  //   console.log(locationHandler);
-  //   console.log("Before API call:", units);
-  const sentRequest = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?${inputLocation}&appid=${appid}&units=${units}`
-  );
-  const gotRequest = await sentRequest.json();
-  console.log(gotRequest);
-};
-function updateApp() {
-  apiRequest(locationHandler, unitsHandler, apiAddress);
-}
-updateApp();
-export { updateApp };
+// const apiRequest = async (inputLocation, units, appid) => {
+//   const sentRequest = await fetch(
+//     `https://api.openweathermap.org/data/2.5/weather?${inputLocation}&appid=${appid}&units=${units}`
+//   );
+//   const gotRequest = await sentRequest.json();
+//   console.log(gotRequest);
+//   if (sentRequest.status === 404) {
+//     window.alert(`Error: ${gotRequest.message}`);
+//   }
+// };
+// function updateApp() {
+//   apiRequest(locationHandler, unitsHandler, apiAddress);
+// }
+// updateApp();
+// export { updateApp };
+
+/* Search by input value */
+const locationInputForm = document.querySelector("[data-location-form]");
+locationInputForm.addEventListener("submit", (e) => {
+  updateApp(e);
+});
+/* Change to units */
+const toggleSwitch = document.querySelector("[data-unit-toggle]");
+toggleSwitch.addEventListener("change", (e) => {
+  updateApp(e);
+});
+
+window.onload = updateApp.apiRequest();
